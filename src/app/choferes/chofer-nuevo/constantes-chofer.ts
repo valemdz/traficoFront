a17da54f07592a3092a518367ed80ca73241ea49
Vale <torresvalemdz@gmail.com>
2018-11-06 14:53:09 -0300

    validacion Fecha Carnet Bis

---
+++ src/app/choferes/chofer-nuevo/constantes-chofer.ts
@@ -0,0 +1,34 @@
+
+export const  DOCUMENTOS: any = [
+  { codigo: 1, descripcion: 'Dni'},
+  { codigo: 2, descripcion: 'Cedula Federal'},
+  { codigo: 3, descripcion: 'Cedula Provincial'},
+  { codigo: 4, descripcion: 'Pasaporte'},
+  { codigo: 5, descripcion: 'Cedula de Extranjeria'},
+  { codigo: 6, descripcion: 'Libreta Enrolamiento'},
+  { codigo: 7, descripcion: 'Documento Unico'},
+  { codigo: 8, descripcion: 'Cedula de Chile'},
+  { codigo: 9, descripcion: 'Salvoconducto'},
+  { codigo: 10, descripcion: 'Cedula de Identidad'},
+  { codigo: 11, descripcion: 'Sin Doc'},
+  { codigo: 12, descripcion: 'Libreta Civica'},
+  { codigo: 13, descripcion: 'Cedula de Identidad Uruguaya'},
+  { codigo: 14, descripcion: 'Carnet de Conducir'}
+];
+
+export const ESTADOS: any = [
+  {codigo: 0, descripcion: 'HABILITADO'},
+  {codigo: 1, descripcion: 'DESHABILITADO'}
+];
+
+export const GRUPOS_SANGUINEOS = [
+  {codigo: 1, descripcion: 'A+'},
+  {codigo: 2, descripcion: 'A-'},
+  {codigo: 3, descripcion: 'B+'},
+  {codigo: 4, descripcion: 'B-'},
+  {codigo: 5, descripcion: 'AB+'},
+  {codigo: 6, descripcion: 'AB-'},
+  {codigo: 7, descripcion: '0+'},
+  {codigo: 8, descripcion: '0-'}
+];
+
