25dd750978ba565d53af6316cdeadbf9909524d8
Vale <torresvalemdz@gmail.com>
2018-11-09 11:04:09 -0300

    enlaces

--- src/app/diagramacion-Servicios/diagramacion-servicio-routing.module.ts
+++ src/app/diagramacion-Servicios/diagramacion-servicio-routing.module.ts
@@ -3,12 +3,14 @@ import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 
 import { DiagramacionListComponent } from './diagramacion-list/diagramacion-list.component';
+import { IdaVtaListComponent } from './ida-vta-list/ida-vta-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild([
-        { path: 'diagramacion', component: DiagramacionListComponent }, 
+        { path: 'diagramacion', component: DiagramacionListComponent },
+        { path: 'idaVtaList', component: IdaVtaListComponent }
          ]) ],
   exports: [RouterModule]
 })
