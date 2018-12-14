534e26365792215629c6f98f92cc5884bef958ae
unknown <torresvalemdz@gmail.com>
2018-12-14 10:08:35 -0300

    detalle de build --prod ok

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  origin: 'http://190.15.196.153:8081/api',
-  originLogin: 'http://190.15.196.153:8081/api/auth/login'
+  origin: 'http://190.15.196.153:8081/nue-trafico-1/api',
+  originLogin: 'http://190.15.196.153:8081/nue-trafico-1/api/auth/login'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
   production: false,
    origin: 'http://127.0.0.1:9966/api',
    originLogin: 'http://127.0.0.1:9966/api/auth/login'
-   //origin: 'http://190.15.196.153:8081/api',
-  //originLogin: 'http://190.15.196.153:8081/api/auth/login'
+   //origin: 'http://190.15.196.153:8081/nue-trafico-1/api',
+   //originLogin: 'http://190.15.196.153:8081/nue-trafico-1/api/auth/login'
 };
